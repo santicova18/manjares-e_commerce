52beadcf8eee20d7b621dc0c07e20896cc33d2ae
Santiago <sebascova18@gmail.com>
2026-03-22 17:56:49 -0500

    refactor(api): remueve importacion de pedido y configuracion de dependencias

--- backend/app/api/dependencies.py
+++ backend/app/api/dependencies.py
@@ -5,14 +5,12 @@ from app.core.connection import get_db
 from app.repo.admin import AdminRepository
 from app.repo.producto_repo import ProductoRepository
 from app.repo.categoria_repo import CategoriaRepository
-from app.repo.configuracion_tienda_repo import ConfiguracionTiendaRepository
-from app.repo.pedido_repo import PedidoRepository
+
 
 from app.services.admin import AdminServices
 from app.services.producto_service import ProductoService
 from app.services.categoria_service import CategoriaService
-from app.services.configuracion_tienda_service import ConfiguracionTiendaService
-from app.services.pedido_service import PedidoService
+
 
 
 # Repositories
@@ -29,12 +27,6 @@ def get_categoria_services(db: Session = Depends(get_db)) -> CategoriaService:
      repo = CategoriaRepository(db)
      return CategoriaService(repo)
 
-def get_configuracion_tienda_services(db: Session = Depends(get_db)) -> ConfiguracionTiendaService:
-     repo = ConfiguracionTiendaRepository(db)
-     return ConfiguracionTiendaService(repo)
 
-def get_pedido_services(db: Session = Depends(get_db)) -> PedidoService:
-     repo = PedidoRepository(db)
-     return PedidoService(repo)
 
 
