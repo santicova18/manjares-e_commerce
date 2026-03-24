683a7330ae2d6e54d545b9a5bd11bd04f513d391
santicova18 <santicova18@gmail.com>
2026-03-24 09:28:46 -0500

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
     server: {
         proxy: {
             '/api': {
-                target: 'http://localhost:8000',
+                target: 'https://manjares-ecommerce-production.up.railway.app/',
                 changeOrigin: true,
             }
         }
