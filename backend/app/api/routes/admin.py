37cd9a096fb8a9bcaf3ed4df12bd85ea04475da5
santicova18 <santicova18@gmail.com>
2026-03-26 11:54:42 -0500

    fix: arregla identacion

--- backend/app/api/routes/admin.py
+++ backend/app/api/routes/admin.py
@@ -5,13 +5,12 @@ from app.services.admin import AdminServices
 from app.core.security import create_access_token
 
 router= APIRouter()
-
 @router.post("/login")
-def login_admin(ad:AdminLogin, services: AdminServices= Depends(get_admin_services)):
-    admin= services.login(ad.email, ad.password)
-  tk = create_access_token({
-    "sub": admin.email,
-    "rol": admin.rol,
-    "nombre": admin.nombre
-})
+def login_admin(ad: AdminLogin, services: AdminServices = Depends(get_admin_services)):
+    admin = services.login(ad.email, ad.password)
+    tk = create_access_token({
+        "sub": admin.email,
+        "rol": admin.rol,
+        "nombre": admin.nombre
+    })
     return {"token": tk}
